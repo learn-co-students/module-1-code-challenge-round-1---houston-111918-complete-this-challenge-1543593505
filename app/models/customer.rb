63d9a60c980a88b2116a4bca81148b1ca827b4c4
github username <github email address>
2018-11-30 11:56:25 -0600

    done

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,12 +1,37 @@
 class Customer
+
   attr_accessor :first_name, :last_name
+
+  @@all = []
 
   def initialize(first_name, last_name)
     @first_name = first_name
     @last_name  = last_name
+    @@all << self
   end
 
-  def full_name
+  def self.all
+    @@all
+  end
+
+  def full_name(first_name, last_name)
     "#{first_name} #{last_name}"
   end
+
+  def reviews
+    Review.all.select do |review|
+      review.customer == self
+    end
+  end
+
+  def restaurants
+    reviews.map do |review|
+      review.restaurant
+    end
+  end
+
+  # def self.add_review
+  #   Review.new #full_name?? self??
+  # end
+
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,18 @@
 class Review
-  
+
+    @@all = []
+
+    attr_accessor :customer, :restaurant
+
+    def initialize(customer, restaurant)
+        @customer = customer
+        @restaurant = restaurant
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
 end
 
